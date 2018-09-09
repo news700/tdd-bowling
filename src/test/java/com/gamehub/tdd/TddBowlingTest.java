d7769deab6c55f6997d9b39197f0a2dd73ac9e37
eddie <eddie.park@mcngamehub.com>
2018-09-09 13:49:48 +0900

    모든 프레임의 결과과 동일한 경우 테스트 시나리오 작성

---
+++ src/test/java/com/gamehub/tdd/TddBowlingTest.java
@@ -0,0 +1,46 @@
+package com.gamehub.tdd;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * roll(int n)과 score()를 구현
+ * 내부적으로 각 프레임(1-10)의 점수를 관리할 것
+ * spare, strike 점수 구현
+ * 범위를 초과했거나 횟수를 초과하여 roll 을 호출하면 IllegalArgumentException 과 IllegalStateException 발생
+ * 최저 점수와 최대 점수까지 테스트에 반드시 반영
+ * 새 테스트 통과마다 commit 해서 github 에 올릴 것
+ * <p>
+ * <p>
+ * 1. 모두 거터인 경우
+ * 2. 모두 오픈인 경우
+ * 3. 모두 스페어(1)인 경우
+ * 4. 모두 스페어(2)인 경우
+ * 5. 모두 스트라이크인 경우
+ */
+public class TddBowlingTest {
+
+	@Test
+	void allGutter() {
+
+	}
+
+	@Test
+	void allOpen() {
+
+	}
+
+	@Test
+	void allSpare1() {
+
+	}
+
+	@Test
+	void allSpare2() {
+
+	}
+
+	@Test
+	void allStrike() {
+
+	}
+}
