074dcab56895892562a7ea35d9475ac8cb40296c
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:21:10 +0900

    record pins by roll

--- src/main/java/com/gamehub/tdd/Game.java
+++ src/main/java/com/gamehub/tdd/Game.java
@@ -2,9 +2,12 @@ package com.gamehub.tdd;
 
 public class Game {
 	private int score = 0;
+	private int[] rolls = new int[21];
+	private int currentRole = 0;
 
 	public void roll(int pins) {
 		score += pins;
+		rolls[currentRole++] = pins;
 	}
 
 	public int score() {
