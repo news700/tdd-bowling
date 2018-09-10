ad828cfeddf37aacc208321d7d966fbf1d7acd21
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:44:28 +0900

    one spare game success

--- src/main/java/com/gamehub/tdd/Game.java
+++ src/main/java/com/gamehub/tdd/Game.java
@@ -12,7 +12,11 @@ public class Game {
 		int score = 0;
 		int i = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			score += rolls[i] + rolls[i + 1];
+			if (rolls[i] + rolls[i + 1] == 10) { //spare
+				score += 10 + rolls[i + 2];
+			} else {
+				score += rolls[i] + rolls[i + 1];
+			}
 			i += 2;
 		}
 		return score;
