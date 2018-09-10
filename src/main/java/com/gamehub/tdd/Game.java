acab3bc9d0981f68091cb6bc1a5bda99107b9e87
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:08:15 +0900

    all one game success

--- src/main/java/com/gamehub/tdd/Game.java
+++ src/main/java/com/gamehub/tdd/Game.java
@@ -1,11 +1,13 @@
 package com.gamehub.tdd;
 
 public class Game {
-	public void roll(int pins) {
+	private int score = 0;
 
+	public void roll(int pins) {
+		score += pins;
 	}
 
 	public int score() {
-		return 0;
+		return score;
 	}
 }
