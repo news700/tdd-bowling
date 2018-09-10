90fe91c703e8832d864b0281dbf16e0355d858d9
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:03:39 +0900

    all gutter game failure

--- src/test/java/com/gamehub/tdd/BowlingGameTest.java
+++ src/test/java/com/gamehub/tdd/BowlingGameTest.java
@@ -2,11 +2,12 @@ package com.gamehub.tdd;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class BowlingGameTest {
 	@Test
 	void allGutterGame() {
 		Game game = new Game();
+		for (int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
 	}
 }
