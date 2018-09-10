dd8c1450633c4f54dec4874791532a2025372f65
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:23:24 +0900

    get score modified

--- src/main/java/com/gamehub/tdd/Game.java
+++ src/main/java/com/gamehub/tdd/Game.java
@@ -1,16 +1,18 @@
 package com.gamehub.tdd;
 
 public class Game {
-	private int score = 0;
 	private int[] rolls = new int[21];
 	private int currentRole = 0;
 
 	public void roll(int pins) {
-		score += pins;
 		rolls[currentRole++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		for (int i = 0; i < rolls.length; i++) {
+			score += rolls[i];
+		}
 		return score;
 	}
 }
