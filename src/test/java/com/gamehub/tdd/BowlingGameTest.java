a6dfea4f7b07ff4b839fb45e87f77019b3105240
eddie <eddie.park@mcngamehub.com>
2018-09-10 21:07:30 +0900

    all one game failure

--- src/test/java/com/gamehub/tdd/BowlingGameTest.java
+++ src/test/java/com/gamehub/tdd/BowlingGameTest.java
@@ -13,4 +13,13 @@ public class BowlingGameTest {
 		}
 		assertThat(game.score()).isEqualTo(0);
 	}
+
+	@Test
+	void allOneGame() {
+		Game game = new Game();
+		for (int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertThat(game.score()).isEqualTo(20);
+	}
 }
